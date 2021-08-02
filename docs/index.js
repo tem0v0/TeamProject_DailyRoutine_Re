ff0aedd920abfbaa3eaf5d8f0381f39255120da0
temro0v0 <merbina@naver.com>
2021-08-02 17:08:37 +0900

    스크롤트리거 1차 완료

---
+++ docs/index.js
@@ -0,0 +1,199 @@
+gsap.to('.overview__content', {
+  x:-200,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.overview__content',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.project-goal__content', {
+  x:200,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.project-goal__content',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.persona__content', {
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.persona__content',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.design-system__logo', {
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.design-system__logo',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.design-system__color', {
+  x:100,
+  opacity:1,
+  duration:2,
+  delay:0.5,
+  scrollTrigger:{
+    trigger:'.design-system__logo',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.design-system__font', {
+  x:-100,
+  opacity:1,
+  duration:2,
+  delay:0.5,
+  scrollTrigger:{
+    trigger:'.design-system__logo',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.wireframe__content', {
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.wireframe__content',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__splash', {
+  x:-200,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__splash',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__home', {
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__home',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__bookmark', {
+  x:130,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__bookmark',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__group', {
+  x:-200,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__group',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__registration', {
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__registration',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__memo-1', {
+  x:200,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__memo-1',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__memo-2', {
+  x:-200,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__memo-2',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__character', {
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__character',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__set', {
+  x:230,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__set',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
+
+gsap.to('.appdesign__popup', {
+  x:-230,
+  opacity:1,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.appdesign__popup',
+    markers:true,
+    start:'0% 50%',
+    end:'100% 0',
+  },
+});
